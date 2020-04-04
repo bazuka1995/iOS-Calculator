5c9fc3dd2e9cf328422b8d14045cfa686cb33b6d
Dov <dov.royal@gmail.com>
2020-04-04 23:17:06 +1100

    Made code more efficient
    made the percentage function more efficient by creating a global operation count variable and counting '+, -, x, /' on each button press

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,18 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var numberOnScreen: Double = 0
-    var previousNumber: Double = 0
-    var performingMath = false
-    //var operation = 0
+    var numberOnScreen: Double = 0 // The number currently on the main label
+    var previousNumber: Double = 0 // The number that was previously on the main label
+    var performingMath = false // Whether the calculator is performing math
     var label2Text: String = "" // Used to store users input and display it on label2
-    var numbers = [String] ()
+    var numbers = [String] () // store each button press in an array for calculating the result
     var sign = false;
     var calcIndex = [Int] ()
-    var plusMinus = 0
-    var divideTimes = 0
+    var plusMinus = 0 // Store number of times the plus or minus symbol is used
+    var divideTimes = 0 // Store the number of times the divide or times symbol is used
     
-    @IBAction func numbers(_ sender: UIButton) {
+    @IBAction func numbers(_ sender: UIButton) { // add button presses (numbers only) to label and label2
         label2Text += String(sender.tag-1)
         label2.text = label2Text
         equals.text = ""
@@ -38,18 +37,12 @@ class ViewController: UIViewController {
     
     @IBAction func percentage(_ sender: UIButton) {
         var temp = ""
-        var operationNum = 0 // Count the number of operations currently on screen
+        var operationNum = plusMinus + divideTimes // The number of operations the user has used
         numberOnScreen = Double(label.text!)!/100 // Divide the number on screen by 100 to create a percentage
         label.text = String(numberOnScreen) // Display the new number on screen
         
-        for char in label2Text { // Count number of operations
-            if char == "+" || char == "/" || char == "x" || char == "-" {
-                operationNum += 1
-            }
-        }
-        
         for char in label2Text { // Rebuild label2 text to include the new percentage
-            if operationNum > 0 {
+            if operationNum > 0 { // append numbers onto label 2 until the last number (which is the new percentage)
                 temp.append(char)
             }
             
@@ -58,7 +51,7 @@ class ViewController: UIViewController {
             }
         }
         
-        temp += label.text!
+        temp += label.text! // append the new percentage
         label2.text = temp
     }
     
@@ -74,25 +67,26 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var equals: UILabel! // show an equals sign on the left when the user presses '='
     
-    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label2: UILabel! // Show button press history till user hits clear
     
-    @IBAction func clear(_ sender: UIButton) {
+    @IBAction func clear(_ sender: UIButton) { // clear everything and reset to zero
         label.text = ""
         previousNumber = 0
         numberOnScreen = 0
-        //operation = 0
         equals.text = ""
         label2.text = ""
         label2Text = ""
         numbers = []
+        plusMinus = 0
+        divideTimes = 0
     }
     
-    @IBAction func buttons(_ sender: UIButton) {
+    @IBAction func buttons(_ sender: UIButton) { // function for divide, multiply, subtract and plus
         
         if label.text != ""  && sender.tag != 16 { // Empty or not equals sign
 
-            previousNumber = Double(label.text!)!
-            numbers.append(String(previousNumber))
+            previousNumber = Double(label.text!)! // store the previous number before the user presses a new number
+            numbers.append(String(previousNumber)) // add the previous number to the numbers array
 
             if sender.tag == 12 { //Divide
                 label.text = "/"
@@ -100,24 +94,28 @@ class ViewController: UIViewController {
                 label2Text += "/" //Concatenating numbers on screen so that they show up in label2
                 label2.text = label2Text
                 numbers.append("/")
+                divideTimes += 1
             } else if sender.tag == 13 { //Multiply
                 label.text = "x"
                 
                 label2Text += "x"
                 label2.text = label2Text
                 numbers.append("x")
+                divideTimes += 1
             } else if sender.tag == 14 { //Subtract
                 label.text = "-"
                 
                 label2Text += "-"
                 label2.text = label2Text
                 numbers.append("-")
+                plusMinus += 1
             } else if sender.tag == 15 { //Add
                 label.text = "+"
                 
                 label2Text += "+"
                 label2.text = label2Text
                 numbers.append("+")
+                plusMinus += 1
             }
 
             //operation = sender.tag
@@ -152,16 +150,11 @@ class ViewController: UIViewController {
     }
     
     
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var label: UILabel! // Main calculator screen
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 }
